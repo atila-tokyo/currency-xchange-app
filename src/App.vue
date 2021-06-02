42f2c2caeb46a88d40bdfebfa738ed514c7621a5
atila-tokyo <atilamanager@gmail.com>
2021-06-02 21:27:00 +0900

    Adding jsconfig file and component ListCurrencies, partially configured axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-<div class="container grid-lg">
-  <h1>
-    Whatever
-  </h1>
+<div class="container grid-lg my-2 py-2">
+  <div class="card">
+    <div class="card-header">
+      <div class="h4">All currencies</div>
+    </div>
+    <div class="card-body">
+
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
-export default { name: 'App' }
+import { onMounted, reactive, toRefs } from 'vue';
+import api from '@/services/api'
+
+export default { 
+  name: 'App',
+  setup() {
+
+    const data = reactive({
+      quotes: {},
+    })
+
+  onMounted(async () => {
+    const response = await api.all();
+    data.quotes = response.data
+  });
+    return { ...toRefs(data) }
+  } 
+}
 </script>
 
 <style>
---
+++ src/components/ListCurrencies.vue
@@ -0,0 +1,10 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <td>Code</td>
+                <td>Name</td>
+            </tr>
+        </thead>
+    </table>
+</template>
