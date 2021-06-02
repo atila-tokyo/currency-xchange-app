94b8071f1fa11f9adaabc8c7b1b6b9add579621c
atila-tokyo <atilamanager@gmail.com>
2021-06-02 18:26:21 +0900

    Adding css and api frameworks

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<div class="container grid-lg">
+  <h1>
+    Whatever
+  </h1>
+</div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default { name: 'App' }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
