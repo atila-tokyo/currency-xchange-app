323245dd7c05b7ee708775923cbdd4a89c1d63c9
atila-tokyo <atilamanager@gmail.com>
2021-06-03 16:51:02 +0900

    adding new component for watchlist of currencies

---
+++ src/components/WatchList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ListCurrencies :quotes="quotes" />
+</template>
+
+<script>
+import ListCurrencies from './ListCurrencies'
+
+export default {
+    components: { ListCurrencies },
+    setup() {
+       const quotes = ref({})
+       
+       
+       return { quotes };
+    }
+}
+</script>
