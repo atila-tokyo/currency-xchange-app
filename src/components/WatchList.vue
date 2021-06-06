ed37541817cc928b12560ffb4d94eb72a8e071f7
atila-tokyo <atilamanager@gmail.com>
2021-06-06 23:24:37 +0900

    Adding setInterval method to restart updating counter

--- src/components/WatchList.vue
+++ src/components/WatchList.vue
@@ -16,20 +16,36 @@ import ListCurrencies from './ListCurrencies'
 import { onMounted, reactive, ref, toRefs, watch } from 'vue';
 import api from '@/services/api';
 
+const CURRENT_UPDATE_TIME = 30;
+
 export default {
     components: { ListCurrencies },
     props: {
         listenQuotations: { type: Array, required: true }
     },
     setup(props, { emit }) {
-    //    const interval = ref(null);
+       const interval = ref(null);
        const quotes = ref({});
-       const nextUpdate = ref(30);
+       const nextUpdate = ref(CURRENT_UPDATE_TIME
+);
 
        const methods = reactive({
            onUnlisten(code) {
                emit('unlisten', code)
            },
+
+           restartInterval() {
+               clearInterval(interval.value);
+               nextUpdate.value = interval.value = CURRENT_UPDATE_TIME
+               interval.value = setInterval(() => {
+                   nextUpdate.value -= 1;
+                   if (nextUpdate.value === 0) {
+                       nextUpdate.value = CURRENT_UPDATE_TIME;
+                       this.refresher();
+                   }
+               })
+           },
+
            async refresher() {
                const { data } = await api.listen(props.listenQuotations);
                quotes.value = data;
